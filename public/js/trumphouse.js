746446f16a6d33db5ef305c5e1694c7ff812222f
Clevor18 <Trev4coleman@gmail.com>
2017-02-01 14:18:40 -0800

    week 4

--- public/js/trumphouse.js
+++ public/js/trumphouse.js
@@ -2,39 +2,91 @@
 * trumphouse -an es5 dop program("div;")
 */
 
-var DOMpiece = function (image) {
-	this.elem = document.createElement("div");
-	this.elem.appendChild(document.createElement("img"));
-}
-var Character = function(name){
+
 		
-}
-var User = function( level ){
-		this.skill = level;
-}
-var trump = function (name){
-		 this.fullname = name
-}
-
-
-var candidate = function (){
-		this.fullname = name;
-}
 var trumphouse = (function() {
 	function init () {
-		console.log('in init');
+		console.log('in init')
+
+		var DOMpiece = function (image) {
+		this.elem = document.createElement("div");
+		this.elem.appendChild(document.createElement("img"));
+}
+var gameObject = function(name, src){
+	this.elem = new DOMpiece(src)
+	this.fullname = name;
+}
+var User = function( level, src ){
+	this.elem = new DOMpiece(src)
+
+	this.skill = level;
+}
+User.prototype = Object.create(gameObject.prototype);
+User.constructor = User;
+
+
+var trump = function(name , src){
+	this.elem = new DOMpiece(src)
+	this.fullname = name;
+}
+
+trump.prototype = Object.create(gameObject.prototype);
+
+trump.constructor= trump;
+
+var candidate = function (name, src){
+	this.fullname = name;
+	this.elem = new DOMpiece(src)
+
+}
+
+		var Beginner = 1;
+		var Advanced = 2;
+
+		var candidate = [];
+
+	
+
 
 		this.candidate = [];
 		this.player = new user();
 
-		this.trump = new trump("Donald J Trump", "img/players/trump.png"); 
-		this.candidate.push(new candidate("Hillary Clinton" ,"img/players/clinton.png"));
-		this.candidate.push(new candidate("Ted Cruz"));
-		this.candidate.push(new candidate("Bernie Sanders"));
-		this.candidate.push(new candidate("Jill Stein"));
-		this.candidate.push(new candidate("Garry Johnson"));
-		this.candidate.push(new candidate("John Kisack"));
-	};
+		var trump = new trump("Donald J Trump", "img/players/trump.png"); 
+		candidate.push(new candidate("Hillary Clinton" ,"img/players/clinton.png"));
+		candidate.push(new candidate("Ted Cruz"));
+		candidate.push(new candidate("Bernie Sanders"));
+		candidate.push(new candidate("Jill Stein"));
+		candidate.push(new candidate("Garry Johnson"));
+		candidate.push(new candidate("John Kisack"));
+	}
+	function getCandidates(){
+		return candidate
+	}
+	function addCandidates(newCandidates){
+		candidate.push
+	}
+	function getTrump(){
+		return trump
+	}
+	function setTrump(newTrump){
+
+	}
+	function getplayer(){
+		return player
+
+
+	} 
+	function setPlayer(newPlayer){
+	
+	}
+});
+
+
+
+
+	
+
+
  /*
  *model
  *store game score
@@ -49,15 +101,8 @@ var trumphouse = (function() {
 *characters
 *the user
 */
-/*
-*controlers
-* detect user pressing arrow keys
-* detect collisoions
-
-*/
 
 
-	return {
-		
-	}
-}());
+
+
+
